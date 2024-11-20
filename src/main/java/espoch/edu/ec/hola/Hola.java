3160b7a382e63c9a82f48a58363d13764be5707b
ShellDomenic <domenicaloor177@gmail.com>
2024-11-20 11:57:57 -0500

    mi primer commit 2

---
+++ src/main/java/espoch/edu/ec/hola/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.hola;
+
+/**
+ *
+ * @author and_j
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
