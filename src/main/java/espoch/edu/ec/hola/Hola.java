b0aa25248e7ab38e7d2ac65795dd8464ea3f4455
ShellDomenic <domenicaloor177@gmail.com>
2024-11-20 12:18:38 -0500

    people

--- src/main/java/espoch/edu/ec/hola/Hola.java
+++ src/main/java/espoch/edu/ec/hola/Hola.java
@@ -11,6 +11,6 @@ package espoch.edu.ec.hola;
 public class Hola {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello people!");
     }
 }
